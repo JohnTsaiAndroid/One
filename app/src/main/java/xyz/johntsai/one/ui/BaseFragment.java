32604e45b76aa5159f36656cf77a6eabb3a431ff
JohnTsai <johntsai.work@gmail.com>
2016-08-08 15:23:57 +0800

    add base fragment

---
+++ app/src/main/java/xyz/johntsai/one/ui/BaseFragment.java
@@ -0,0 +1,20 @@
+package xyz.johntsai.one.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by JohnTsai(mailto:johntsai.work@gmail.com) on 16/8/8.
+ */
+public class BaseFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
